3e099bb4659b617246a38f5da3827f993549f4a7
Yoshy0777 <139258576+Yoshy0777@users.noreply.github.com>
2023-07-20 09:50:58 -0600

    Mayor_Menor_Edad.py

--- Main.py
+++ Main.py
@@ -1,2 +1,7 @@
 nombre = input("Cual es tu nombre?: ")
 edad = int(input("Cual es tu edad?: ")
+
+if(edad>=18):
+           print(nombre+" Eres mayor de edad")
+else:
+           print(nombre+" Eres menor de edad")
